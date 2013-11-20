fed4bed4b16609eca42ac6731af8fc9e60727d2b
e-nikolov <emil.e.nikolov@gmail.com>
2013-11-20 12:12:26 +0100

    Update Some.java

--- src/main/java/org/enacer/opt/Some.java
+++ src/main/java/org/enacer/opt/Some.java
@@ -1,8 +1,9 @@
 package org.enacer.opt;
 
 /**
- * todo
- */
+* With permission from http://www.edc4it.com/2011/08/02/a-scala-style-option-class-for-java/
+*
+*/
 public class Some<T> extends Option<T> {
 	public Some(T value) {
 		super(value);
